10c1ccdcfbc625477c7326fe98d6e7fabd39d7d3
Yousef Dawood <yousefdawood31@gmail.com>
2026-07-04 14:00:42 +0300

    add and configure resend

--- src/lib/env.ts
+++ src/lib/env.ts
@@ -5,6 +5,7 @@ export const env = createEnv({
   server: {
     BETTER_AUTH_URL: z.url(),
     BETTER_AUTH_SECRET: z.string().min(16).max(128),
+    RESEND_API_KEY: z.string().min(16).max(128),
   },
 
   experimental__runtimeEnv: {},
