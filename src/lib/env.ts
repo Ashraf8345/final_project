4dc3afd7b25c48613c267d01582dc5629b9f9a14
Yousef Dawood <yousefdawood31@gmail.com>
2026-07-04 13:47:51 +0300

    initialize t3 env

---
+++ src/lib/env.ts
@@ -0,0 +1,11 @@
+import { createEnv } from "@t3-oss/env-nextjs";
+import * as z from "zod";
+
+export const env = createEnv({
+  server: {
+    BETTER_AUTH_URL: z.url(),
+    BETTER_AUTH_SECRET: z.string().min(16).max(128),
+  },
+
+  experimental__runtimeEnv: {},
+});
